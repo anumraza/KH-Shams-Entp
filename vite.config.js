a66da664826af786577cbebbec3e731bc8b4d05a
Anum Fatima <anumraza8194@gmail.com>
2025-09-21 20:39:00 +0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+    base: "/KH-Shams-Entp/", 
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
- base: '/KH-Shams-Entp/',
 });
